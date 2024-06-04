f572b80928bb8ce1644bf2dbd93e485b6b2ebe68
catid <mrcatid@gmail.com>
2024-06-04 01:58:49 +0000

    wip

--- dataset/cpp_dataloader/setup.py
+++ dataset/cpp_dataloader/setup.py
@@ -3,6 +3,7 @@ from setuptools.command.build_ext import build_ext
 import os
 import subprocess
 import sys
+import multiprocessing
 
 class CMakeExtension(Extension):
     def __init__(self, name):
@@ -23,9 +24,14 @@ class CMakeBuild(build_ext):
         cfg = "Debug" if self.debug else "Release"
         build_args = ["--config", cfg]
 
+        num_threads = multiprocessing.cpu_count() - 1
+        if num_threads < 1:
+            num_threads = 1
+        jthreadsarg = f"-j{num_threads}"
+
         os.makedirs(self.build_temp, exist_ok=True)
         subprocess.check_call(["cmake", "-S", ".", "-B", self.build_temp] + cmake_args)
-        subprocess.check_call(["cmake", "--build", self.build_temp] + build_args)
+        subprocess.check_call(["cmake", "--build", self.build_temp, jthreadsarg] + build_args)
 
 setup(
     name="cpp_dataloader",
@@ -33,6 +39,6 @@ setup(
     ext_modules=[CMakeExtension("cpp_dataloader")],
     cmdclass={"build_ext": CMakeBuild},
     package_data={
-        'my_package': ['cpp_dataloader.so'],
+        'cpp_dataloader': ['cpp_dataloader.so'],
     },
 )
