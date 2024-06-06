d06a25745ea4ee46d3bd9e1ca73a9a17d81fb9b4
catid <mrcatid@gmail.com>
2024-06-06 03:37:51 +0000

    wip

--- dataset/shard_dataset.py
+++ dataset/shard_dataset.py
@@ -7,7 +7,7 @@ import glob
 import argparse
 import tiktoken
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from tqdm import tqdm
+import sys
 
 def get_parquet_files(directory):
     parquet_files = []
@@ -43,13 +43,24 @@ def process_shard(data_prep, df, args, tokenizer):
         for future in as_completed(futures):
             future.result()  # Raise exceptions if any
 
+def print_progress_bar(args, iteration, total, start_time, bar_length=40):
+    percent = f"{100 * (iteration / float(total)):.1f}"
+    elapsed_time = time.time() - start_time
+    eta = (elapsed_time / iteration) * (total - iteration) if iteration > 0 else 0
+    elapsed_time_str = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
+    eta_str = time.strftime("%H:%M:%S", time.gmtime(eta))
+    bar_filled = int(bar_length * iteration // total)
+    bar = '#' * bar_filled + '-' * (bar_length - bar_filled)
+    sys.stdout.write(f"\r[Ranks {args.rank_start}-{args.rank_start + args.rank_count - 1}] {percent}% [{bar}] {elapsed_time_str} / {eta_str}")
+    sys.stdout.flush()
+
 def main():
     parser = argparse.ArgumentParser(description="Read and process shards of a Parquet file.")
     parser.add_argument('--dataset_dir', type=str, default="/mnt/Media/datasets/fineweb-edu", help="Path to the Parquet files.")
     parser.add_argument('--rank_start', type=int, default=0, help="First rank for this node.")
     parser.add_argument('--rank_count', type=int, default=1, help="Number of shards for this node.")
     parser.add_argument('--world_size', type=int, default=1, help="Total number of shards.")
-    parser.add_argument('--output_dir', type=str, default="~/dataset_shard", help="Output directory.")
+    parser.add_argument('--output_dir', type=str, default="dataset_shard", help="Output directory.")
 
     args = parser.parse_args()
 
@@ -58,17 +69,23 @@ def main():
     print(f"Processing shards {args.rank_start} to {args.rank_start + args.rank_count - 1} of {args.world_size} across {args.rank_count} nodes.")
 
     # Initialize the tokenizer
-    tokenizer = tiktoken.encoding_for_model("gpt-4o")
+    tokenizer = tiktoken.encoding_for_model("gpt-4")
 
     parquet_files = get_parquet_files(args.dataset_dir)
 
     data_prep = DataPreparation(args.output_dir)
 
-    for file_path in tqdm(parquet_files, desc="Processing Parquet files"):
+    total_files = len(parquet_files)
+    start_time = time.time()
+
+    for i, file_path in enumerate(parquet_files):
         df = read_parquet(file_path)
         process_shard(data_prep, df, args, tokenizer)
 
+        print_progress_bar(args, i + 1, total_files, start_time)
+
     data_prep.destroy()
+    print("\nProcessing complete.")
 
 if __name__ == "__main__":
     main()
